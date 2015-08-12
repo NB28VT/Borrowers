e7b77ebfabc5c0f847ed2bc24bebe0ebb964d059
Nathan <Burgess@Nathans-MacBook-Pro.local>
2015-08-12 11:20:42 -0400

    Added new create friend route and controllers

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+    actions: {
+    save() {
+      console.log('+- save action bubbled up to friends route');
+      return false;
+    },
+    cancel () {
+      console.log('+- cancel action bubbled up to friends route');
+      return false;
+    }
+
+
+  }
+
 });
